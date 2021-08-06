e328f6523b9e31813ef77a0b76dd251430219650
Yu Chen <yu.chen@live.ie>
2021-08-06 10:10:54 +0800

    sep.

--- __tests__/fmt-spec.ts
+++ __tests__/fmt-spec.ts
@@ -1,7 +1,7 @@
 import { fmt } from '../src/fmt';
 import { LLVM_Darwin } from 'smake';
 import { addLibs } from '@smake/libs';
-import { resolve, sep } from 'path';
+import { resolve } from 'path';
 
 test('fmt', () => {
   class A extends LLVM_Darwin {
@@ -9,10 +9,7 @@ test('fmt', () => {
   }
   const B = addLibs(A, fmt, fmt, fmt);
   const b = new B();
-  const p = resolve(__dirname, '..', 'fmt', 'include').replace(
-    new RegExp(sep, 'g'),
-    '/'
-  );
+  const p = resolve(__dirname, '..', 'fmt', 'include').replace(/\\/g, '/');
   expect(b.sysIncludedirs.includes(p)).toBe(true);
   expect(b.cxxflags.includes('-DFMT_HEADER_ONLY')).toBe(true);
   expect(b.sysIncludedirs.length).toBe(3);
